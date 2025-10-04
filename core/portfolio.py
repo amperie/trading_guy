860300a2e845ec6af3120e47a3c7a7bf8fd87493
amperie <pablo.i.salanova@gmail.com>
2025-10-04 09:12:43 -0700

    First

---
+++ core/portfolio.py
@@ -0,0 +1,11 @@
+"""
+Portfolio class
+- Stores configuration about the portfolio strategy and priorities for each security
+- Tracks what the portfolio is comprised of
+- Takes signals as input and adjusts the portfolio
+Interfaces:
+- Initialize
+- create_orders: takes signal inputs and creates a list of orders
+    - Keeps a history of orders and signals
+    - Has an OrderManager member to handle the orders
+"""
